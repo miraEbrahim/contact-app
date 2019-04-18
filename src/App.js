2992b2f451f1e28532dd6b2d50a3a8c7a84371ad
Maryam Ebrahim <maryam@miras-mbp.home>
2019-04-18 14:17:47 +0100

    move contacts array to src/db/contacts.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacs'
-const contacts = [
-  {
-    id: 'ryan',
-    name: 'Ryan Florence',
-    email: 'ryan@reacttraining.com',
-    avatarURL: 'http://localhost:5001/ryan.jpg'
-  },
-  {
-    id: 'michael',
-    name: 'Michael Jackson',
-    email: 'michael@reacttraining.com',
-    avatarURL: 'http://localhost:5001/michael.jpg'
-  },
-  {
-    id: 'tyler',
-    name: 'Tyler McGinnis',
-    email: 'tyler@reacttraining.com',
-    avatarURL: 'http://localhost:5001/tyler.jpg'
-  }
-];
+import contacts from './db/contacts.js'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts />
+        <ListContacts contacts={contacts} />
       </div>
     );
   }
--- src/ListContacs.js
+++ src/ListContacs.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class ListContacts extends Component {
-  //the only property on our component class that we alwayse have to specift
   render() {
-    console.log('props', this.props)
-    return <ol className="contact-list" />;
+    
+    return 
+      <ol className="contact-list">
+        { this.props.contacts.map(
+          (contact) => (
+            <li key={contact.id}>
+              {contact.name}
+            </li>
+          )
+        )}
+      </ol>
   }
 }
 
