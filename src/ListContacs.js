f574b0a3fe9c1401b6d41851f55cf72a9e234648
Maryam Ebrahim <maryam@miras-mbp.home>
2019-04-18 14:06:43 +0100

    add a conatctlist props

--- src/ListContacs.js
+++ src/ListContacs.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class ListContacts extends Component {
   //the only property on our component class that we alwayse have to specift
   render() {
+    console.log('props', this.props)
     return <ol className="contact-list" />;
   }
 }
