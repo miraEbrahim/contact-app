23cdddedcda941d5b1ce39d1542ba80f56e5a229
Maryam Ebrahim <maryam@miras-mbp.home>
2019-04-18 13:44:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+const contacts = [
+  {
+    id: 'ryan',
+    name: 'Ryan Florence',
+    email: 'ryan@reacttraining.com',
+    avatarURL: 'http://localhost:5001/ryan.jpg'
+  },
+  {
+    id: 'michael',
+    name: 'Michael Jackson',
+    email: 'michael@reacttraining.com',
+    avatarURL: 'http://localhost:5001/michael.jpg'
+  },
+  {
+    id: 'tyler',
+    name: 'Tyler McGinnis',
+    email: 'tyler@reacttraining.com',
+    avatarURL: 'http://localhost:5001/tyler.jpg'
+  }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/ListContacs.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ListContacts extends Component {
+  //the only property on our component class that we alwayse have to specift
+  render() {
+    return <ol className="contact-list" />;
+  }
+}
+
+export default ListContacts;
