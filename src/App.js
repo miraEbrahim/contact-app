8a285c6745fd22dec9098f927c73409883a4996a
Maryam Ebrahim <maryam@miras-mbp.home>
2019-04-20 01:38:23 +0100

    use contacts apis remove() method to update the backend

--- src/App.js
+++ src/App.js
@@ -19,8 +19,9 @@ class App extends Component {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
     })) 
-  }
 
+  ContactsAPI.remove(contact)
+  }
   render() {
     return (
       <div>
