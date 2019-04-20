3b795f78670bfd6480ddf65aa08957394ffca592
Maryam Ebrahim <maryam@miras-mbp.home>
2019-04-20 01:34:04 +0100

    set this.state.contacts to empty [] in app.js , add componentdidmount event in app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacs'
+import * as ContactsAPI from './utils/ContactAPI' 
 import './index.css'
 
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        id: 'ryan',
-        name: 'Ryan Florence',
-        email: 'ryan@reacttraining.com',
-        avatarURL: 'http://localhost:5001/ryan.jpg'
-      },
-      {
-        id: 'michael',
-        name: 'Michael Jackson',
-        email: 'michael@reacttraining.com',
-        avatarURL: 'http://localhost:5001/michael.jpg'
-      },
-      {
-        id: 'tyler',
-        name: 'Tyler McGinnis',
-        email: 'tyler@reacttraining.com',
-        avatarURL: 'http://localhost:5001/tyler.jpg'
-      }
-    ]
+    contacts: []
+  }
+//use componentDidMount for AJAX API requests
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({contacts})
+    })
   }
 
   removeContact = (contact) => {
